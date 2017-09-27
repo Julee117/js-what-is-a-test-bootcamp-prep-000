c10c41a28d8e0b402dd7f03db59de40ab521ebf8
Julee117 <juliannne@gmail.com>
2017-09-27 17:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ describe('Name', () => {
 
   describe("Height", () => {
       it("is less than 40", () => {
-        expect(index.height).toBeLessThan(40)
+        expect(height).toBeLessThan(40)
       })
     })
 
