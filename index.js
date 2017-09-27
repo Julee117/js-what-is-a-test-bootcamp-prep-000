5e5b5b9b1613b0fbaff01cbf9f99e2b1bddf3c3e
Julee117 <juliannne@gmail.com>
2017-09-27 17:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
-var name = "Susan"
+var index.name = "Susan"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
-console.log(name)
+console.log(index.name)
 console.log(message)
 
 describe('Name', () => {
